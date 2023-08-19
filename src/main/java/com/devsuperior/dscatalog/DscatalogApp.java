c21ba4b6d773c3432f6c76dddf52a1c781495061
danielfragareis <danielfreis91@gmail.com>
2023-08-18 22:15:00 -0300

    Category resource

---
+++ src/main/java/com/devsuperior/dscatalog/DscatalogApp.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApp {
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApp.class, args);
+	}
+}
