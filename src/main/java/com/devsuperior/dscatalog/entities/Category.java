938cff2e0ef15be66ccd3bcd46eb4bc533ff243c
danielfragareis <danielfreis91@gmail.com>
2023-08-19 08:41:35 -0300

    Category resource, category service e configuração banco h2

--- src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -3,12 +3,24 @@ package com.devsuperior.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	// Interface Serializable funciona para que seja possível converter 
 	// uma classe em bytes e que possa ser retransmitida por dispositivos
 	// de entrada e saída, como redes por exemplo
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
---
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscatalog.services;
+//import org.springframework.*;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+@Service	
+//Podem ser usadas outras anotations a depender do objetivo, como: @Component e @Repository
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	/*
+	 * Usada para realizar a injeção de dependência automática. Ela é usada para
+	 * indicar que uma determinada variável precisa ser automaticamente conectada a
+	 * um bean (objeto gerenciado pelo Spring) adequado durante a inicialização da
+	 * aplicação.
+	 */
+	@Autowired
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
